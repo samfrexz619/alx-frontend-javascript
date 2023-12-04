bb59ff66b4fcd9d49094a659518422aa94f3205a
samfrexz619 <samfrexz69@gmail.com>
2023-12-04 08:33:20 +0100

    add 0-get_list_students.js

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,21 @@
+const arrObjs = [
+  {
+    id: 1,
+    firstName: 'Guillaume',
+    location: 'San Francisco',
+  },
+  {
+    id: 2,
+    firstName: 'James',
+    location: 'Columbia',
+  },
+  {
+    id: 3,
+    firstName: 'Serena',
+    location: 'San Francisco',
+  },
+];
+
+const getListStudents = () => arrObjs;
+
+export default getListStudents;
