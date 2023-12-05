59f0a18ec4e7fa5efcb9abe69c586053dbac3868
samfrexz619 <samfrexz69@gmail.com>
2023-12-05 15:54:28 +0100

    add files

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,22 @@
+const cleanSet = (set, startString) => {
+  if (
+    !set && !startString && !(set instanceof Set) && typeof startString !== 'string'
+  ) {
+    return '';
+  }
+
+  const parts = [];
+
+  for (const value of set.values()) {
+    if (typeof value === 'string' && value.startsWith(startString)) {
+      const valueSubStr = value.substring(startString.length);
+
+      if (valueSubStr && valueSubStr !== value) {
+        parts.push(valueSubStr);
+      }
+    }
+  }
+  return parts.join('-');
+};
+
+export default cleanSet;
---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,13 @@
+const groceriesList = () => {
+  const vals = [
+    ['Apples', 10],
+    ['Tomatoes', 10],
+    ['Pasta', 1],
+    ['Rice', 1],
+    ['Banana', 5],
+  ];
+
+  return new Map(vals);
+};
+
+export default groceriesList;
